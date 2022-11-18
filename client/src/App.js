19d0a2d4f5cb0f39b05846ef76559b3e56c1c266
Nicolas Stabilini <stabilini@gmail.com>
2022-11-17 21:33:59 -0300

    fondos terminados

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage/LandingPage.jsx';
@@ -6,10 +7,14 @@ import Home from './components/Home/Home.jsx';
 import Detail from './components/Detail/Detail.jsx'
 import CreateActivity from './components/CreateActivity/CreateActivity.jsx'
 
+import styles from './App.module.css';
 import './App.css';
 
 function App() {
+  const theme = useSelector(state => state.theme);
+  
   return (
+    <div className={ `${styles.background} ${styles[theme]}` }>
       <Switch>
         <Route exact path='/'>
           <LandingPage />
@@ -24,6 +29,7 @@ function App() {
           <CreateActivity />
         </Route>
       </Switch>
+    </div>
   );
 }
 
