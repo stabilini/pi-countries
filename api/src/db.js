cdf84bd56f4bbf25f20fdbf1507f74a220f1fb65
Nicolás Darío Alfonso Stabilini <36341963+stabilini@users.noreply.github.com>
2023-01-19 10:58:31 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -10,6 +10,13 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  // Para DB deploy. Suelen pedir conexión ssl.
+  // dialectOptions: {
+  //   ssl: true
+  // },
+  define: {
+    freezeTableName: true // Para que no le cambie el nombre a todas las tablas
+  }
 });
 const basename = path.basename(__filename);
 
