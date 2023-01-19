25d05d932e4385b3bfd41c4bbe23d610398c298c
Nicolás Darío Alfonso Stabilini <36341963+stabilini@users.noreply.github.com>
2023-01-19 11:53:27 -0300

    Update LoadingSpinner.jsx

--- client/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -4,13 +4,14 @@ import { useSelector } from 'react-redux';
 import styles from "./LoadingSpinner.module.css";
 import logo from "../../assets/logo-globe.png";
 
-const LoadingSpinner = ({text}) => {
+const LoadingSpinner = ({text, textsmall}) => {
   const theme = useSelector(state => state.theme);
 
   return (
     <div className={ `${styles.spinnerContainer} ${styles[theme]}` }>
       <img className={ `${styles.spinnerLoading} ${styles[theme]}` } src={logo} alt='rotating globe' />
       <div className={ `${styles.text} ${styles[theme]}` }>{text}</div>
+      { textsmall ? <div className={ `${styles.text} ${styles.textsmall} ${styles[theme]}` }>{textsmall}</div> : <></> }
     </div>
   );
 }
