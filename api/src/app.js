09b5f6c0eb504ad1d4b8d3a7c356698c48d84918
Nicolás Darío Alfonso Stabilini <36341963+stabilini@users.noreply.github.com>
2023-01-19 10:37:50 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -18,9 +18,9 @@ server.use(cookieParser());
 // server.use(morgan('dev')); // deshabilitado para que los tests no salgon entrecortados
 server.use((req, res, next) => {
   // DEPLOY DESCOMENTAR SIGUIENTE LINEA
-  // res.header('Access-Control-Allow-Origin', 'https://pi-countries-client-production-08bf.up.railway.app'); // * para aceptar conexiones de cualquier lado
+  res.header('Access-Control-Allow-Origin', 'https://pi-countries-5uga.onrender.com');
   // DEVELOPMENT DESCOMENTAR SIGUIENTE LINEA
-  res.header('Access-Control-Allow-Origin', '*'); // * para aceptar conexiones de cualquier lado
+  // res.header('Access-Control-Allow-Origin', '*'); // * para aceptar conexiones de cualquier lado
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
